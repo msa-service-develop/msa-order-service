e89616aa1e37610d01cdaa038e646b117d7d6f46
dkswjdrbs12@naver.com <dkswjdrbs12@naver.com>
2022-10-25 22:41:22 +0900

    Initial commit

---
+++ src/test/java/study/msa/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.msa.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
